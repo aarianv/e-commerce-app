fea5ac3515b699fa9d1aa3f7cc4bc9f2b5ed195a
kerosensei <aarianvara7@gmail.com>
2024-02-06 22:26:17 +0000

    yes

--- HTML/js/scripts.js
+++ HTML/js/scripts.js
@@ -7,20 +7,20 @@ function closeNav() {
 }
 
 const products = [{
-  image: 'imgs\tabi boots.jpg', name: 'maison  margiela  black  tabi  boots', rating: {
+  image: 'imgs/tabi-boots.jpg', name: 'maison  margiela  black  tabi  boots', rating: {
     stars: 4.5,
     count: 76,
   }, type: 'new',
   pricePence: 95000
 }, {
-  image: 'imgs\le fleur cardigan.jpg', name: 'lactose  x  le  fleur  striped  cardigan', rating: {
+  image: 'imgs/le-fleur-cardigan.jpg', name: 'lactose  x  le  fleur  striped  cardigan', rating: {
     stars: 5.0,
     count: 38
   }, type: 'new',
   pricePence: 50000
 }, {
-  image: 'imgs\1017 alyx 9sm bracelet.jpg', name: '1017  alyx  9sm  buckle  bracelet  34', rating: {
-    stars: 3.9,
+  image: 'imgs/1017-alyx-9sm-bracelet.jpg', name: '1017  alyx  9sm  buckle  bracelet  34', rating: {
+    stars: 4.0,
     count: 98
   }, type: 'new',
   pricePence: 29500
@@ -42,7 +42,7 @@ products.forEach((product) => {
 
       <div class="product-rating-container">
         <img class="product-rating-stars"
-          src="images/ratings/rating-${product.rating.stars * 10}.png">
+          src="imgs/ratings/rating-${product.rating.stars * 10}.png">
         <div class="product-rating-count link-primary">
           ${product.rating.count}
         </div>
