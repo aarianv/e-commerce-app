3ab12b96ecc05a611827debfabed02cc54ff0582
kerosensei <aarianvara7@gmail.com>
2024-02-07 19:29:22 +0000

    items added

--- HTML/datasets/products.js
+++ HTML/datasets/products.js
@@ -15,5 +15,29 @@ export const products = [{
       stars: 4.0,
       count: 98
     }, type: 'new',
-    pricePence: 29500
+    pricePence: 29500 
+  }, {
+    image: 'imgs/rick-owens-pink-bolans.jpeg', name: 'rick  owens  pink  bolan  jeans', rating: {
+      stars: 4.5,
+      count: 78,
+    }, type: 'new',
+    pricePence: 68500
+  }, {
+    image: 'imgs/cdg-bomber.jpg', name: 'comme  des  garçons  homme  bomber  jacket', rating: {
+      stars: 4.0,
+      count: 17,
+    }, type: 'new',
+    pricePence: 76500
+  }, {
+    image: 'imgs/rick-kiss-heels.jpg', name: 'rick  owens  kiss  heels', rating: {
+      stars: 5.0,
+      count: 191
+    }, type: 'new',
+    pricePence: 100000
+  }, {
+    image: 'imgs/maison-mihara.jpg', name: 'maison  mihara  yasuhiro', rating: {
+      stars: 3.5,
+      count: 122
+    }, type: 'new',
+    pricePence: 31300
   }];
