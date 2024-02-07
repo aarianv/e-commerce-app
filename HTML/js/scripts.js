80fe217097182d5bc3b1bec6b1551b1f8a7f43e9
kerosensei <aarianvara7@gmail.com>
2024-02-07 17:17:29 +0000

    yes!

--- HTML/js/scripts.js
+++ HTML/js/scripts.js
@@ -70,13 +70,13 @@ products.forEach((product) => {
       <div class="product-spacer"></div>
 
       <div class="added-to-cart">
-        <img src="images/icons/checkmark.png">
-        Added
+        <img src="imgs/icons/checkmark.png">
+        added
       </div>
 
       <button class="add-to-cart-button button-primary js-add-to-cart"
       data-product-id="${product.id}">
-        Add to Cart
+        add to cart
       </button>
     </div>
   `;
@@ -85,3 +85,29 @@ products.forEach((product) => {
 console.log(productsHTML)
 
 document.querySelector('.js-products-grid').innerHTML = productsHTML;
+
+document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+
+      let matchingItem;
+
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem = item;
+        }
+      });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+          productId: productId, 
+          quantity: 1
+        })
+      }
+
+      console.log(cart);
+    });
+  });
